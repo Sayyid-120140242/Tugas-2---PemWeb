91a0f6f896acbc9e8ce1bd7ac2047e5b31c71a87
Azrasyd <105223430+Sayyid-120140242@users.noreply.github.com>
2022-10-07 03:21:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tugas 2 PemWeb</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form class ="input-box" action="action.php" method="get">
+    <center><table class="table-box"border="1">
+        <tr>
+            <td colspan="3"><center>Brand</center></td>
+        </tr>
+        <tr>
+            <td>ID Pelanggan</td>
+            <td>:</td>
+            <td><input type="text" name="id"></td>
+        </tr>
+        <tr>
+            <td>Nama Produk</td>
+            <td>:</td>
+            <td><input type="text" name="nama"></td>
+        </tr>
+        <tr>
+            <td colspan="3"><center><button>Submit</button></center></td>
+            </form>
+        </tr>
+    </table></center>
+    </div>
+</body>
+</html>
